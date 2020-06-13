02d35a01768369c658bfd53b7e0e16957034d00e
unknown <dgloria.web@gmail.com>
2020-06-13 06:34:01 +0100

    first

---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::view('/','welcome');
+Route::view('contact','contact');
+Route::view('about','about');
+
+Route::get('customers','CustomersController@list');
