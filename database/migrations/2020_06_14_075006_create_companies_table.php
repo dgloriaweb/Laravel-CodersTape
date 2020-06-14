6cb6e43d08a4f24a99f5b8f74470a2f6b4ce0ed9
unknown <dgloria.web@gmail.com>
2020-06-14 08:57:42 +0100

    added stubs

---
+++ database/migrations/2020_06_14_075006_create_companies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->increments('id'); //for id
+            $table->string('name');
+            $table->string('phone');
+			$table->timestamp('deleted_at')->nullable();
+			$table->timestamp('created_at')->useCurrent();
+			$table->timestamp('updated_at')->default(DB::raw('NULL ON UPDATE CURRENT_TIMESTAMP'))->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
