7180fc7f320650d5d2e8505d780cfe7cd02c2c85
unknown <dgloria.web@gmail.com>
2020-06-15 06:51:01 +0100

    chg

--- resources/views/customers/form.blade.php
+++ resources/views/customers/form.blade.php
@@ -41,7 +41,7 @@
     <label for="company_id">Company</label>
     <select name="company_id" id="company_id" class="form-control">
         @foreach ($companies as $company)
-        <option value="{{ $company->id }}">{{ $company->name }}</option>
+        <option value="{{ $company->id }}" {{ $company->id  == $customer->company_id ? 'selected' : '' }}> {{ $company->name }}</option>
         @endforeach
     </select>
 </div>
