a3111969cf49815918afcaa4bb68026f3a76b643
unknown <dgloria.web@gmail.com>
2020-06-16 20:48:41 +0100

    added command

--- resources/views/customers/form.blade.php
+++ resources/views/customers/form.blade.php
@@ -2,26 +2,16 @@
     <label for="name">
         Name
     </label>
-    <input
-        class="form-control"
-        type="text"
-        name="name"
-        placeholder="name"
-        value="{{ old('name') ?? $customer->name }}"
-    />
+    <input class="form-control" type="text" name="name" placeholder="name"
+        value="{{ old('name') ?? $customer->name }}" />
     <div>
         {{ $errors->first('name') }}
     </div>
 </div>
 <div class="form-group">
     <label for="email">Email</label>
-    <input
-        class="form-control"
-        type="email"
-        name="email"
-        placeholder="email"
-        value="{{ old('email') ?? $customer->email }}"
-    />
+    <input class="form-control" type="email" name="email" placeholder="email"
+        value="{{ old('email') ?? $customer->email }}" />
     <div>
         {{ $errors->first('email') }}
     </div>
@@ -33,9 +23,7 @@
         <option value="" disabled>Select customer status</option>
         @foreach($customer->isActiveOptions() as $key => $value)
 
-        <option value="{{ $key }}" {{$customer-
-            >is_active == $value ? 'selected' :''}}>{{ $value }}</option
-        >
+        <option value="{{ $key }}" {{$customer->is_active == $value ? 'selected' :''}}>{{ $value }}</option>
         @endforeach
     </select>
 </div>
@@ -43,10 +31,8 @@
     <label for="company_id">Company</label>
     <select name="company_id" id="company_id" class="form-control">
         @foreach ($companies as $company)
-        <option value="{{ $company->id }}" {{ $company-
-            >id == $customer->company_id ? 'selected' : '' }}>
-            {{ $company->name }}</option
-        >
+        <option value="{{ $company->id }}" {{ $company->id == $customer->company_id ? 'selected' : '' }}>
+            {{ $company->name }}</option>
         @endforeach
     </select>
 </div>
